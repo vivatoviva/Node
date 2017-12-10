b4e1398f0ba74204948f03a8d4038bfb4d05c913
vivatoviva <ligen_cqupt@163.com>
2017-12-10 21:22:01 +0800

    xiaoxi

--- hello.js
+++ hello.js
@@ -1,3 +1,13 @@
-const path = require('path');
+
+function ha(){
+    console.log('jajaj');
+    setTimeout(() => {
+        console.log('hahahhah');
+        console.log('nihao!')
+    }, 500);
+}
+ha();
+
+
 
 
